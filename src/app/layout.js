aae2719ece90f4377117d771f9db07e66b1de1c7
codewithsaidul <codewithsaidul@gmail.com>
2024-09-03 01:30:58 +0600

    Responsive Navbar Added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-// import "./venobox.min.css"
+
 import Navbar from "@/component/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className}`}>
-        <div className="mx-auto flex md:w-full w-[90%]  max-w-screen-xl flex-col justify-between gap-4 p-4 lg:gap-6 lg:p-6">
+        <div className="mx-auto flex md:w-full max-w-screen-xl flex-col justify-between gap-4 p-4 lg:gap-6 lg:p-6">
             <Navbar />
 
             {children}
