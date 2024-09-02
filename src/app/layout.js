bcea133585b2ddcf7e03c6c916f3d679eb5f5cf4
codewithsaidul <codewithsaidul@gmail.com>
2024-09-03 00:50:46 +0600

    NextJs Personal Portfolio Navbar UI Added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+// import "./venobox.min.css"
+import Navbar from "@/component/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className}`}>
+        <div className="mx-auto flex md:w-full w-[90%]  max-w-screen-xl flex-col justify-between gap-4 p-4 lg:gap-6 lg:p-6">
+            <Navbar />
+
+            {children}
+        </div>
+      </body>
     </html>
   );
 }
